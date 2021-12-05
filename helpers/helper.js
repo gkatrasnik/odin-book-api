cf58f024f0a7974a3b7a19ad1b9e1c1b4429bc30
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-12-05 13:20:39 +0100

    jwt token doesnt expire

--- helpers/helper.js
+++ helpers/helper.js
@@ -5,18 +5,15 @@ const secretKey = process.env.SECRET;
 
 function issueJWT(user) {
   const _id = user._id;
-  const expiresIn = "1d";
 
   const payload = {
     sub: _id,
     iat: Date.now(),
   };
 
-  const signedToken = jsonwebtoken.sign(payload, secretKey, {
-    expiresIn: expiresIn,
-  });
+  const signedToken = jsonwebtoken.sign(payload, secretKey);
 
-  return { token: signedToken, expires: expiresIn };
+  return { token: signedToken };
 }
 
 module.exports.issueJWT = issueJWT;
