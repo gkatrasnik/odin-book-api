4f1dae5a82689a883f222c65a09ff6716edc9b2d
Gasper Katrasnik <gkatrasnik@gmail.com>
2021-12-18 11:44:00 +0100

    accept friend request controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -173,7 +173,7 @@ exports.send_friend_request_POST = async (req, res) => {
         msg: "You can not send friend request to your self",
       });
     } else if (
-      secondUser.friend_requests.includes(currentUser._id.toString())
+      secondUser.recieved_friend_requests.includes(currentUser._id.toString())
     ) {
       return res.status(400).json({
         success: false,
@@ -187,7 +187,7 @@ exports.send_friend_request_POST = async (req, res) => {
     } else {
       //if everything is ok, add ids to friend requests
       currentUser.sent_friend_requests.push(secondUser._id);
-      secondUser.friend_requests.push(currentUser._id);
+      secondUser.recieved_friend_requests.push(currentUser._id);
 
       await currentUser.save();
       await secondUser.save();
@@ -204,3 +204,57 @@ exports.send_friend_request_POST = async (req, res) => {
 };
 
 // accept friend request
+
+exports.accept_friend_request_POST = async (req, res) => {
+  const { userId } = req.body;
+  const secondUserId = req.params.userId;
+
+  try {
+    const currentUser = await User.findById(userdId);
+    const secondUser = await User.findById(secondUserId);
+
+    if (
+      !currentUser.recieved_friend_requests.includes(secondUser._id) ||
+      !secondUser.sent_friend_requests.includes(loggedUser._id)
+    ) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          msg: "You do not have friend request from this user",
+        });
+    } else {
+      //if everything is ok
+
+      // delete ids from recieved friend request and sent friend requests
+      const updatedRecievedFriendRequests =
+        currentUser.recieved_friend_requests.filter(
+          (request) => request != secondUser._id.toString()
+        );
+      const updatedSentFriendRequests = secondUser.sent_friend_requests.filter(
+        (request) => request != currentUser._id.toString()
+      );
+
+      currentUser.recieved_friend_requests = updatedRecievedFriendRequests;
+      secondUser.sent_friend_requests = updatedSentFriendRequests;
+
+      // add ids to friends for both users...
+      const currentUsersFriends = [...currentUser.friends, secondUser._id];
+      const secondUsersFriends = [...secondUser.friends, currentUser._id];
+      currentUser.friends = currentUsersFriends;
+      secondUser.friends = secondUsersFriends;
+
+      const updatedCurrentUser = await currentUser.save();
+      const updatedSecondUser = await secondUser.save();
+
+      return res.status(200).json({
+        success: true,
+        msg: "Accepted friend request",
+        currentUser: updatedCurrentUser,
+        secondUser: updatedSecondUser,
+      });
+    }
+  } catch (err) {
+    return res.status(400).json({ msg: err.message });
+  }
+};
